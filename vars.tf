9979730717c4572c10d1be4b211d3ce4a25d13a2
Justin Hauer <justinhauer@gmail.com>
2020-05-23 10:52:18 -0500

    testing out tf

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY_ID" {
+    type        = string
+    description = "access key id to AWS"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    type        = string
+    description = "secret access key"
+}
